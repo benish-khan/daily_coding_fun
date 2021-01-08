0258e94b1294fb1e6e5d8cf1c2231c0d3696c768
Benish Khan <bsarinelli@gmail.com>
2021-01-07 22:39:56 -0800

    Adding remainder functions.

---
+++ remainder.py
@@ -0,0 +1,10 @@
+# Write your remainder function here:
+def remainder(num1, num2):
+  first_input = num1 * 2
+  second_input = num2 / 2
+  return (first_input % second_input)
+# Uncomment these function calls to test your remainder function:
+print(remainder(15, 14))
+# should print 2
+print(remainder(9, 6))
+# should print 0
