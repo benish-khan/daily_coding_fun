4d5888ec35b88640d99854ea70ac8a12d760ef20
Benish Khan <bsarinelli@gmail.com>
2021-01-07 22:01:23 -0800

    committing tenth power function

---
+++ tenth_pow.py
@@ -0,0 +1,12 @@
+def tenth_power(num):
+	return num ** 10
+
+#tenth_power function:
+print(tenth_power(10))
+# 10 to the 10th power is 10000000000
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
