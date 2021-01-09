749866dedae67dd5fd77c052b8f91f9f89a38fb6
Benish Khan <bsarinelli@gmail.com>
2021-01-09 00:19:43 -0800

    Adding multiples function

---
+++ mul_of_inputs.py
@@ -0,0 +1,15 @@
+# Define the function header to accept one input parameter called num
+# Print out 1 times num
+# Print out 2 times num
+# Print out 3 times num
+# Return the value of 3 times num
+def first_three_multiples(num): 
+    a = num * 1
+    print(a)
+    b = num * 2
+    print(b) 
+    c = num * 3
+    print(c)
+    return(c)
+    
+print(first_three_multiples(2))
