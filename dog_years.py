1942026ad18cfcc445aeb0a75615db05a832e33d
Benish Khan <bsarinelli@gmail.com>
2021-01-09 01:10:35 -0800

    add that dog year age

---
+++ dog_years.py
@@ -0,0 +1,6 @@
+def dog_years(name, age):
+    dog_age = age * 7 
+    return name + " , you are " + str(dog_age) + " years old in dog years."
+
+print(dog_years("benish", 33))
+# should print benish , you are 231 years old in dog years.
