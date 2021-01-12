6267ca57c138af5fff5e611532932e7f1e9ff420
Benish Khan <bsarinelli@gmail.com>
2021-01-12 11:27:37 -0800

    Completed function to test if a number fall within a certain range

---
+++ in_range.py
@@ -0,0 +1,16 @@
+# Testing if a number falls within a certain range
+# Define the function to accept three numbers as parameters
+# Test if the number is greater than or equal to the lower bound and less than or equal to the upper bound
+# If this is true, return True, otherwise, return False
+
+# Write your in_range function here:
+def in_range(num,lower,upper):
+    if num >= lower and num <= upper:
+      return True
+    else:
+      return False
+# Uncomment these function calls to test your in_range function:
+print(in_range(10, 10, 10))
+# should print True
+print(in_range(5, 10, 20))
+# should print False
