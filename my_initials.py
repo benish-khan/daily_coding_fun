57f529cd112d43e66a85d26e37a731ce2b8803a8
Benish Khan <bsarinelli@gmail.com>
2021-01-15 23:22:00 -0800

    Add function to assess if two nums do not equal 10

--- my_initials.py
+++ my_initials.py
@@ -89,6 +89,39 @@ def divisible_by_ten(num):
 
 
 
+# Create a function named not_sum_to_ten() that has two parameters named num1 and num2.
+
+# Return True if num1 and num2 do not sum to 10. Return False otherwise.
+
+def not_sum_to_ten(num1, num2):
+	if num1 + num2 != 10:
+		return True
+	else:
+		return False
+
+print(not_sum_to_ten(9, -1)) # should print True
+print(not_sum_to_ten(9, 1)) # should print False
+print(not_sum_to_ten(5,5)) # should print False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
