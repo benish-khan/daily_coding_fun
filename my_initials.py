605fbdb64200554346f4311f91c49654798ceb14
Benish Khan <bsarinelli@gmail.com>
2021-01-15 23:04:44 -0800

    Adding test for zero division error

--- my_initials.py
+++ my_initials.py
@@ -17,3 +17,18 @@ def initials():
 # B   B  B   B
 # BBBB   BBBB
 initials()
+
+def divide_two_numbers(x, y):
+  result = x / y
+  return result
+ 
+try:
+  result = divide_two_numbers(2,0)
+  print(result)
+except NameError:
+  print("A NameError occurred.")
+except ValueError:
+  print("A ValueError occurred.") 
+except ZeroDivisionError:
+  print("A ZeroDivisionError occurred.")
+ 
