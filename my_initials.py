e3a7fa6105b8c18a354b39e61e08a8c1958925d8
Benish Khan <bsarinelli@gmail.com>
2021-01-15 23:10:21 -0800

    Add over budget function

--- my_initials.py
+++ my_initials.py
@@ -16,19 +16,33 @@ def initials():
 # B   B  B   B
 # B   B  B   B
 # BBBB   BBBB
-initials()
+# initials()
 
-def divide_two_numbers(x, y):
-  result = x / y
-  return result
+# def divide_two_numbers(x, y):
+#   result = x / y
+#   return result
  
-try:
-  result = divide_two_numbers(2,0)
-  print(result)
-except NameError:
-  print("A NameError occurred.")
-except ValueError:
-  print("A ValueError occurred.") 
-except ZeroDivisionError:
-  print("A ZeroDivisionError occurred.")
+# try:
+#   result = divide_two_numbers(2,0)
+#   print(result)
+# except NameError:
+#   print("A NameError occurred.")
+# except ValueError:
+#   print("A ValueError occurred.") 
+# except ZeroDivisionError:
+#   print("A ZeroDivisionError occurred.")
  
+
+ # Write your over_budget function here:
+def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
+	if budget < (food_bill + electricity_bill + internet_bill +rent):
+		return True
+	else:
+		return False
+
+
+# Uncomment these function calls to test your over_budget function:
+print(over_budget(100, 20, 30, 10, 40))
+# should print False
+print(over_budget(80, 20, 30, 10, 30))
+# should print True
