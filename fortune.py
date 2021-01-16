5ca5963b856ee422e585d966c76c693e83913cde
Benish Khan <bsarinelli@gmail.com>
2021-01-16 14:38:13 -0800

    Add billionaire question function

--- fortune.py
+++ fortune.py
@@ -14,3 +14,28 @@ elif fortune == 3:
   print("You can only connect the dots looking backwards")
 elif fortune == 4:
   print("The fortune you seek is in another cookie")
+
+
+  # Who Wants To Be A Millionaire 💰 
+
+score = 0
+
+option1 = 'Fresca'
+option2 = 'V8'
+option3 = 'something'
+option4 = 'A&W'
+  
+print("For ordering his favorite beverages on demand, LBJ had four buttons installed in the Oval Office labeled 'Coffee', 'Tea', 'Coke', and what?\n")
+
+print("A.", option1)
+print("B.", option2)
+print("C.", option3)
+print("D.", option4)
+  
+answer = 'a'
+
+if answer == 'A' or answer == 'a': 
+  score += 100
+  print("\nCorrect!")
+else:
+  print("\nNope, sorry!")
