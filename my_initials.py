d061de9c26b5d987860ca8dac50705173c108d2a
Benish Khan <bsarinelli@gmail.com>
2021-01-15 23:14:11 -0800

    Add twice as big function

--- my_initials.py
+++ my_initials.py
@@ -16,11 +16,12 @@ def initials():
 # B   B  B   B
 # B   B  B   B
 # BBBB   BBBB
+#function call below
 # initials()
 
-# def divide_two_numbers(x, y):
-#   result = x / y
-#   return result
+def divide_two_numbers(x, y):
+  result = x / y
+  return result
  
 # try:
 #   result = divide_two_numbers(2,0)
@@ -42,7 +43,23 @@ def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
 
 
 # Uncomment these function calls to test your over_budget function:
-print(over_budget(100, 20, 30, 10, 40))
+# print(over_budget(100, 20, 30, 10, 40))
 # should print False
-print(over_budget(80, 20, 30, 10, 30))
+# print(over_budget(80, 20, 30, 10, 30))
+# should print True
+
+# Create a function named twice_as_large() that has two parameters named num1 and num2.
+
+# Return True if num1 is more than double num2. Return False otherwise.
+
+def twice_as_large(num1, num2):
+	if num1 > (2 * num2):
+		return True
+	else:
+		return False
+
+#your twice_as_large function:
+#print(twice_as_large(10, 5))
+# should print False
+#print(twice_as_large(11, 5))
 # should print True
