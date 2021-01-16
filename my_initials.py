bd0d128b2ff189386252e227967e70807646f6a2
Benish Khan <bsarinelli@gmail.com>
2021-01-15 22:43:47 -0800

    my initials in block letters

---
+++ my_initials.py
@@ -0,0 +1,19 @@
+def initials():
+  print("BBBB" + "  " + " BBBB") 
+  print("B" + "   " + "B" + "  " + "B" + "   " + "B" )
+  print("B" + "   " + "B" + "  " + "B" + "   " + "B" )
+  print("BBBB" + "  " + " BBBB")
+  print("B" + "   " + "B" + "  " + "B" + "   " + "B" )
+  print("B" + "   " + "B" + "  " + "B" + "   " + "B" )
+  print("BBBB" + "  " + " BBBB")
+
+#call the function which should print "BB" to the screen in block letters!
+
+# BBBB   BBBB
+# B   B  B   B
+# B   B  B   B
+# BBBB   BBBB
+# B   B  B   B
+# B   B  B   B
+# BBBB   BBBB
+initials()
