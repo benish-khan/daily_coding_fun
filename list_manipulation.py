adb5d584bf28b07ee000578fa797fe255e42ac69
Benish Khan <bsarinelli@gmail.com>
2021-01-17 22:05:09 -0800

    Practice with lists

---
+++ list_manipulation.py
@@ -0,0 +1,20 @@
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+
+names_and_dogs_names = zip(names, dogs_names)
+
+list_of_names_and_dogs_names = list(names_and_dogs_names)
+
+print(list_of_names_and_dogs_names)
+
+first_names = ['Ainsley', 'Ben', 'Chani', 'Depak']
+
+age = []
+
+age.append(42)
+
+all_ages = [32, 41, 29] + age
+
+name_and_age = zip(first_names, age)
+
+ids = range(4)
