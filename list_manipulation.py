e7ed0408c2b2d81e5216a843ba31e287a41177aa
Benish Khan <bsarinelli@gmail.com>
2021-01-17 22:15:17 -0800

    Add gradebook project

--- list_manipulation.py
+++ list_manipulation.py
@@ -18,3 +18,19 @@ all_ages = [32, 41, 29] + age
 name_and_age = zip(first_names, age)
 
 ids = range(4)
+
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+subjects.append("computer science")
+
+grades = [98, 97, 85, 88]
+grades.append(100)
+gradebook = list(zip(grades, subjects))
+gradebook.append(("93", "visual arts"))
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook
+
+print(full_gradebook)
