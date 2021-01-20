5fb556373ae11ef8d75e8ae524c1cf549fd4af49
Benish Khan <bsarinelli@gmail.com>
2021-01-19 21:59:13 -0800

    Adding gradebook function

--- credits.py
+++ credits.py
@@ -13,3 +13,21 @@ def graduation_reqs(gpa, credits):
 
   elif credits < 120 and gpa < 2.0:
     return "You do not meet either requirement to graduate!"
+
+    last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+subjects.append("computer science")
+
+grades = [98, 97, 85, 88]
+grades.append(100)
+gradebook = list(zip(grades, subjects))
+gradebook.append(("93", "visual arts"))
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook
+
+print(full_gradebook)
+
+
