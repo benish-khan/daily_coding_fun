c771843b3eb4abeaaf3512da2fadff483b80ef83
Benish Khan <bsarinelli@gmail.com>
2021-01-21 23:00:36 -0800

    Submitting code for pizza list exercise

---
+++ pizza.py
@@ -0,0 +1,26 @@
+# Pizza Exercise 
+toppings = ["pepperone", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_pizzas = len(toppings)
+
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizzas = list(zip(toppings, prices))
+
+print(pizzas)
+
+pizzas.sort()
+
+
+cheapest_pizzas = pizzas[0]
+
+priciest_pizza = pizzas[-1]
+
+three_cheapest = pizzas[-3:]
+
+print(three_cheapest)
+
+num_two_dollar_slices = prices.count(2)
+
