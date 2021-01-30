d3a00b22751e459299d213601fce124ca0257f12
Benish Khan <bsarinelli@gmail.com>
2021-01-29 23:52:55 -0800

    while logic to add

--- while_pops.py
+++ while_pops.py
@@ -1,8 +1,10 @@
+
 all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
 students_in_poetry = []
 
 while len(students_in_poetry) < 6:
   student = all_students.pop()
   students_in_poetry.append(student)
-  
 print(students_in_poetry)
+
+#expected result of this is "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"
