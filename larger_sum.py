2108dc5c382432806460ef51bd9513dd8e17421b
Benish Khan <bsarinelli@gmail.com>
2021-01-31 23:00:12 -0800

    rework reversed_list function

--- larger_sum.py
+++ larger_sum.py
@@ -52,3 +52,12 @@ def same_values(lst1, lst2):
 
 #Uncomment the line below when your function is done
 print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+def reversed_list(lst1, lst2):
+  for index in range(len(lst1)):
+    if lst1[index] != lst2[len(lst2) - 1 - index]:
+      return False
+  return True
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
