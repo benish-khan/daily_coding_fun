5d74830ff1b9570b4ae602e09da1088549627ac8
Benish Khan <bsarinelli@gmail.com>
2021-01-31 22:53:40 -0800

    Adding in the same_values function

--- larger_sum.py
+++ larger_sum.py
@@ -42,3 +42,13 @@ def max_num(nums):
 
 #Uncomment the line below when your function is done
 print(max_num([50, -10, 0, 75, 20]))
+
+def same_values(lst1, lst2):
+  new_lst = []
+  for item in range(len(lst1)):
+    if lst1[item] == lst2[item]:
+      new_lst.append(item)
+  return new_lst
+
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
