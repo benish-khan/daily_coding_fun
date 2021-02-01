22be054b2d026427f822de2b1ed0c6940711b079
Benish Khan <bsarinelli@gmail.com>
2021-01-31 22:35:40 -0800

    Adding over_nine_thousand function

--- larger_sum.py
+++ larger_sum.py
@@ -11,3 +11,24 @@ def larger_sum(lst1, lst2):
 print(larger_sum([1, 9, 5], [2, 3, 7])) #expect to print lst1
 print(larger_sum([2, 4, 9], [1, 9, 5],)) #expect to print lst2
 print(larger_sum([2, 3, 7], [2, 3, 7],)) #expect to print lst1
+
+
+
+print(over_nine_thousand([8000, 900, 120, 5000]))
+def over_nine_thousand(lst):
+  count = 0
+  for i in lst:
+    count += i
+    if count > 9000:
+      break
+    elif count < 9000:
+      return sum(lst)
+    elif len(lst) < 1:
+      return 0
+  return count
+
+
+
+#print(over_nine_thousand([8000, 900, 120, 5000])) #expecting 9020
+#print(over_nine_thousand([])) #expecting 0
+#print(over_nine_thousand([8000, 120])) expecting 8120
