f8a8d8c31a2f85b3176b8b26ae3c54942266019b
Benish Khan <bsarinelli@gmail.com>
2021-02-01 22:04:13 -0800

    Adding in password generator function

--- name_setup.py
+++ name_setup.py
@@ -8,3 +8,12 @@ def account_generator(first_name, last_name):
 new_account = account_generator(first_name, last_name)
 
 print(new_account)
+
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
