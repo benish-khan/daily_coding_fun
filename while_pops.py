2d451c76a82f5e97715110665522a6b8d5eb9e2b
Benish Khan <bsarinelli@gmail.com>
2021-02-03 22:34:42 -0800

    adding in while loop with append step

--- while_pops.py
+++ while_pops.py
@@ -2,9 +2,18 @@
 all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
 students_in_poetry = []
 
+'''
 while len(students_in_poetry) < 6:
   student = all_students.pop()
   students_in_poetry.append(student)
 print(students_in_poetry)
 
+'''
+
 #expected result of this is "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"
+
+while len(students_in_poetry) < 6:
+  student = all_students.pop()
+  students_in_poetry.append(student + "What's up!")
+print(students_in_poetry)
+
