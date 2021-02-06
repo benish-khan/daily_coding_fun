c8859197676def3dda8ff67cb0290b1a0ba8ddb4
Benish Khan <bsarinelli@gmail.com>
2021-02-05 22:24:44 -0800

    strings practice

---
+++ str.py
@@ -0,0 +1,25 @@
+txt = "She said \"never let go\"."  # should print She said "never let go".
+print(txt)
+
+
+name ="saudah"
+
+print("b" in name) #return False
+print("u" in name) #return True
+
+a = "me"
+b = " she"
+c = a + b
+print(c) # print me she
+
+str = "car"
+for c in str:
+    print(c + c.upper() + c + c.upper())
+
+    """
+    cCcC
+    aAaA
+    rRrR
+    """
+
+    
