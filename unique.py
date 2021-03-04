cdd5f228fec15705d1faaf289ae95bb98b46525d
Benish Khan <bsarinelli@gmail.com>
2021-03-03 21:41:51 -0800

    char x function

--- unique.py
+++ unique.py
@@ -12,3 +12,17 @@ print(unique_english_letters("mississippi"))
 # should print 4
 print(unique_english_letters("Apple"))
 # should print 4
+
+
+def count_char_x(word, x):
+  count_x = 0
+  for letter in word:
+    if letter == x:
+      count_x += 1
+  return count_x
+
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
