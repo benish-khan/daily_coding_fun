79cc94687be623177762f15332a09ee77d07771a
Benish Khan <bsarinelli@gmail.com>
2021-03-03 21:52:50 -0800

    Add in the iss function

--- unique.py
+++ unique.py
@@ -26,3 +26,14 @@ print(count_char_x("mississippi", "s"))
 # should print 4
 print(count_char_x("mississippi", "m"))
 # should print 1
+
+# Write your count_multi_char_x function here:
+def count_multi_char_x(word, x):
+  splits = word.split(x)
+  return(len(splits)-1)
+
+# Uncomment these function calls to test your function:
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+print(count_multi_char_x("apple", "pp"))
+# should print 1
